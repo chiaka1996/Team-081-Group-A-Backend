d931fdbfb92a6e05c344361b043352cad1a2abe0
newton <chiakajunior@yahoo.com>
2020-09-28 11:36:30 +0100

    making changes

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const http = require("http");
+// const app = require("./app");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-    res.end("this is my server response!");
+const cors = require("cors");
+
+const mongoose = require("mongoose");
+
+// const {MongoClient} = require("mongodb");
+
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.set("port", port);
+
+app.use(cors());
+
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
+const connection = mongoose.connection;
+connection.once('open',(res) => {
+    console.log("MongoDB connected");
 });
 
-server.listen(process.env.PORT || 5000);
+const api = require("./routes");
+
+app.use("/apis", api);
+
+const server = http.createServer(app);
+
+server.listen(port);
